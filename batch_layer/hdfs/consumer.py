206b7bb97e83eb2ae8c66f79fa6be79c6a77aebc
Anzhe Meng <meng@prepbaseballreport.com>
2024-11-20 14:43:02 -0500

    completed hdfs ingestion

--- batch_layer/hdfs/consumer.py
+++ batch_layer/hdfs/consumer.py
@@ -1,6 +1,6 @@
 from confluent_kafka import Consumer, KafkaException
 from hdfs.client import InsecureClient
-import time
+import time, json
 
 # Kafka Consumer Configuration
 consumer_conf = {
@@ -15,7 +15,6 @@ consumer.subscribe([TOPIC])
 # HDFS Configuration
 hdfs_client = InsecureClient('http://localhost:9870', user='memphis')  # Replace with your HDFS Namenode URL
 hdfs_dir = "/user/memphis/warehouse"
-file_name = f"{hdfs_dir}/output_{int(time.time())}.json"
 
 # Ensure HDFS directory exists
 if not hdfs_client.content(hdfs_dir, strict=False):
@@ -24,24 +23,26 @@ if not hdfs_client.content(hdfs_dir, strict=False):
 # Start consuming Kafka messages and writing to HDFS
 try:
     print(f"Starting to consume from Kafka topic: {TOPIC}")
-    batch, batch_size = [], 10
-    with hdfs_client.write(file_name, encoding='utf-8') as hdfs_file:
-        while True:
-            msg = consumer.poll(timeout=1.0)
-            if msg is None:
-                continue
-            if msg.error():
-                if msg.error().code() == KafkaException._PARTITION_EOF:
-                    print(f"End of partition reached {msg.partition()}")
-                else:
-                    print(f"Error: {msg.error()}")
-                continue
+    batch, batch_size = [], 100
+    
+    while True:
+        msg = consumer.poll(timeout=1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            if msg.error().code() == KafkaException._PARTITION_EOF:
+                print(f"End of partition reached {msg.partition()}")
+            else:
+                print(f"Error: {msg.error()}")
+            continue
 
-            batch.append(msg.value().decode('utf-8'))
-            if len(batch) >= batch_size:
-                hdfs_file.write('\n'.join(batch) + '\n')
+        batch.extend(json.loads(msg.value()))
+        if len(batch) >= batch_size:
+            file_name = f"{hdfs_dir}/output_{int(time.time() * 1000)}.json"
+            with hdfs_client.write(file_name, encoding='utf-8') as hdfs_file:
+                hdfs_file.write(json.dumps(batch))
                 batch = []
-                print(f"Written to HDFS!")
+                print(f"Written {file_name} to HDFS!")
 
 except KeyboardInterrupt:
     print("\nStopping consumer...")
